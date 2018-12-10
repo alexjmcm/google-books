4f427a4e6221f4d4c22979b8878354a9cad3996a
Alex Monteiro <alexjmcm@Alexs-MacBook-Pro.local>
2018-12-09 22:41:49 -0500

    Final Updates

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navbar, NavItem } from "react-materialize";
 
 export default () => (
-    <Navbar brand="Google Books" className="teal lighten-2" right fixed>
+    <Navbar brand="Google Books React App" className="blue lighten-2" right fixed>
         <NavItem href="/search">Search</NavItem>
         <NavItem href="/saved">Saved Books</NavItem>
     </Navbar>
